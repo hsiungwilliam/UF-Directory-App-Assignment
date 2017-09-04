51be565799b6f6b542c82e3797bab7f4fbb84100
hsiungwilliam <hsiung.william.ib@gmail.com>
2017-09-04 12:12:01 -0400

    Second commit

--- listingController.js
+++ listingController.js
@@ -7,8 +7,26 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+
+     $scope.showListingDetail = false;
+    $scope.addListing = function(code, name, latitude, longitude, address) {
+       $scope.temp = {
+        "code": code,
+        "name": name,
+        "coordinates": {
+          "latitude": latitude, 
+          "longitude": longitude,
+        }, 
+        "address": address
+       };
+       $scope.listings.push($scope.temp);
+    };
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index,1);
+    };
+    $scope.showDetails = function(index) {
+      $scope.showListingDetail = true;
+      $scope.current = $scope.listings[index];
+    };
   }
 ]);
